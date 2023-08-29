75ac54b6d9cd8ed60daa09d2d32b2e8f3ce28963
mikeylia <143411255+mikeylia@users.noreply.github.com>
2023-08-29 06:43:58 +0300

    Update geisha.js

--- geisha.js
+++ geisha.js
@@ -1163,6 +1163,7 @@ async function addBot(botAmount) {
       const randomIndex = Math.floor(Math.random() * (name.length + 1));
       const randomCharacter = getRandomCharacter();
       modifiedName = name.slice(0, randomIndex) + randomCharacter + name.slice(randomIndex);
+	}
 
 
 
